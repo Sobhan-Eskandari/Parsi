2c92b42c139f416378f65a00e5fee1329eb87b7e
Mersede <mersede.rahbar@gmail.com>
2017-06-23 14:38:04 +0430

    fixed

--- resources/views/components/java.blade.php
+++ resources/views/components/java.blade.php
@@ -1,16 +1,20 @@
-<div class="col">
+<div class="col-3">
     <div class="card">
-    <a href="{{ route('show_post', $id) }}">
+        <a href="{{ route('show_post', $id) }}">
 
-        @if(strlen($photo) === 0)
-            <img src="{{ asset('images/java.jpg') }}" alt="{{ $title }}" class="mx-auto d-block img-fluid card-img-top" height="100%" width="100%">
-        @else
-            <img src="{{ asset('images' . '/' . $photo) }}" alt="{{ $title }}" class="mx-auto d-block img-fluid">
-        @endif
-            <div class="card-block px-1 py-3">
-                <h5 class="card-text pt-2" style="{{$style}}">{{ $title }}</h5>
-                <p class="card-text">نوشته شده در: {{ $date }}</p>
+            @if(strlen($photo) === 0)
+                <img src="{{ asset('images/java.jpg') }}" alt="{{ $title }}"
+                     class="mx-auto d-block card-img-top img-fluid" height="300px" width="300px">
+            @else
+                <img src="{{ asset('images' . '/' . $photo) }}" alt="{{ $title }}" class="mx-auto d-block img-fluid"
+                     height="300px" width="300px">
+            @endif
+            <div class="row justify-content-center">
+                <div class="card-block px-3 py-3">
+                    <h5 class="card-text pt-2" style="{{$style}}">{{ $title }}</h5>
+                    <p class="card-text">نوشته شده در: {{ $date }}</p>
+                </div>
             </div>
-    </a>
+        </a>
     </div>
 </div>
